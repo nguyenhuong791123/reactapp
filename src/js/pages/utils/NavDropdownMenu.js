c6fe9a25beec2f4a982529b974eab102b5915122
nguyenhuong791123 <nguyenhuong791123@gmail.com>
2019-08-16 07:52:27 +0000

    Menu Drop

--- src/js/pages/utils/NavDropdownMenu.js
+++ src/js/pages/utils/NavDropdownMenu.js
@@ -2,6 +2,8 @@ import React, { Component as C } from 'react';
 import onClickOutside from 'react-onclickoutside'
 import { NavDropdown } from 'react-bootstrap';
 
+import TwoMenu from './Alert';
+
 class NavDropdownMenu extends C {
   constructor(props) {
     super(props);
@@ -9,7 +11,15 @@ class NavDropdownMenu extends C {
     this._onShow = this._onShow.bind(this);
     this._onClick = this._onClick.bind(this);
 
-    this.state = { isShow: false }
+    this.state = {
+      show: true
+      ,objs: [
+        { view: 0, target: 'target_00', label: 'label_00' }
+        ,{ view: 1, target: 'target_01', label: 'label_01' }
+        ,{ view: 0, target: 'target_02', label: 'label_02' }
+        ,{ view: 1, target: 'target_03', label: 'label_03' }
+      ]
+    }
   }
 
   _onClick(e) {
@@ -23,31 +33,40 @@ class NavDropdownMenu extends C {
       if(obj.tagName != 'A') return;
     }
     const flag = parseInt(obj.getAttribute("view"));
-    console.log(flag==1);
+
     if(flag === 1) {
       e.preventDefault();
       e.stopPropagation();  
-      this.setState({ isShow: true });
+      this.setState({ show: true });
+      this.forceUpdate();
     }
   }
 
   _onShow(e) {
-    this.setState({ isShow: !this.state.isShow });
+    this.setState({ show: !this.state.show });
+  }
+
+  _getTowMenu() {
+    return( <TwoMenu show={ this.state.show } top={ 100 } left={ 100 } objs={ this.state.objs } /> );
   }
 
   handleClickOutside = () => {
-    this.setState({ isShow: false });
+    this.setState({ show: false });
   }
 
   render() {
-      return ( 
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown" show={ this.state.isShow } onClick={ this._onShow.bind(this) }>
-            <NavDropdown.Item onClick={ this._onClick.bind(this) } view="0">Action</NavDropdown.Item>
-            <NavDropdown.Item onClick={ this._onClick.bind(this) } view="1">Another action</NavDropdown.Item>
-            <NavDropdown.Item onClick={ this._onClick.bind(this) } view="0">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item onClick={ this._onClick.bind(this) } view="1">Separated link</NavDropdown.Item>
+    var towMenu = this._getTowMenu();
+    return (
+      <div>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown" show={ this.state.show } onClick={ this._onShow.bind(this) }>
+          <NavDropdown.Item onClick={ this._onClick.bind(this) } view="0">Action</NavDropdown.Item>
+          <NavDropdown.Item onClick={ this._onClick.bind(this) } view="1">Another action</NavDropdown.Item>
+          <NavDropdown.Item onClick={ this._onClick.bind(this) } view="0">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item onClick={ this._onClick.bind(this) } view="1">Separated link</NavDropdown.Item>
         </NavDropdown>
+        { towMenu }
+      </div>
     );
   }
 }
